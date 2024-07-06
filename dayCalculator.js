82941b5e35958e938137b3d4fdd78a5ad9d193a9
Karen Mui <karen_mui@hotmail.com>
2024-07-06 02:38:20 -0400

    debugged

--- dayCalculator.js
+++ dayCalculator.js
@@ -5,29 +5,33 @@ function calculateDayInYear(date) {
   const day = Number(splitDate[2]);
 
   const validMonth = function(month) {
-    return month && month >= 1 && month < 12;
+    return month && month >= 1 && month <= 12;
   }
 
   const validDay = function(month, day) {
-    return day && day >= 1 && day < DAYS_IN_MONTH[month - 1];
+    return day && day >= 1 && day <= DAYS_IN_MONTH[month - 1];
   }
 
   const calculateDayNumber = function(month, day) {
-    let dayOfYear = 1;
+    let dayOfYear = 0;
 
-    for (let i = 1; i < month; i++) {
-      dayOfYear += DAYS_IN_MONTH[i - 1];
+    for (let i = 0; i < (month - 1); i++) {
+      dayOfYear += DAYS_IN_MONTH[i];
     }
+    dayOfYear += day
 
     return dayOfYear;
   }
 
   const daysInFeb = function(year) {
+    if (isLeapYear(year)) {
+      return 29;
+    }
     return 28;
   }
 
   const isLeapYear = function(year) {
-    return isMultiple(year, 400) || !isMultiple(year, 100) && isMultiple(year, 4);
+    return (isMultiple(year, 400)) || (isMultiple(year, 4) && !isMultiple(year, 100));
   }
 
   const DAYS_IN_MONTH = [31, daysInFeb(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
